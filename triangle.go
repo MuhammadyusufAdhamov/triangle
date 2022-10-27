a7c439fa00a3789ed89f8a9a4abe0baf9547e02c
MuhammadyusufAdhamov <muhammadyusufadhamov0@gmail.com>
2022-10-27 07:57:48 +0500

    second commit

--- triangle.go
+++ triangle.go
@@ -15,22 +15,3 @@ func isTriangle(a,b,c int) bool {
 	}
 	return false
 }
-
-
-package main
-
-import "fmt"
-
-func main(){
-	a,b,c := 3,4,5
-	
-	fmt.Println(isTriangle(a,b,c))
-
-}
-
-func isTriangle(a,b,c int) bool {
-	if c*c == a*a + b*b {
-		return true
-	}
-	return false
-}
